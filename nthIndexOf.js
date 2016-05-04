6dd018a4a50794fa38cfe14e5387c10b85461687
Lau Yu Hei <yuhlau@gmail.com>
2016-05-04 13:57:09 +0800

    Update to 1.0.1

--- nthIndexOf.js
+++ nthIndexOf.js
@@ -1,12 +1,13 @@
 "use strict";
 /**
  * String.nthIndexOf
- * Search for the nth occurrence of a substring in a string
+ * Search for the nth occurrence of a substring in a string. Support both 
+ * forward and backward searching
  *
  * Copyright (c) 2016 Lau Yu Hei
  * 
  * @author Lau Yu Hei
- * @version 1.0.0
+ * @version 1.0.1
  * @license The MIT License (MIT)
  * https://opensource.org/licenses/MIT
  **/
@@ -17,8 +18,10 @@
      * value in a string.
      *
      * @param {string} searchValue  The value to search for
-     * @param {number} nthIndex     Which occurence to search for, index
-     *                              starts with st 1
+     * @param {number} nthIndex     Which occurence to search for. If index 
+     *                              is a negative number, it will perform
+     *                              a backward search. All index starts at
+     *                              1
      * @param {number} [start]      (Optional) At which position to start
      * @return {number}             Position where the search value of nth 
      *                              occurrence appears, -1 if it does not 
@@ -26,20 +29,44 @@
      */
     String.prototype.nthIndexOf = function(searchValue, nthIndex, start) {
         var i, l, 
-            tmp;
+            tmp, position=[], posIndex=0, targetIndex;
         
-        if (nthIndex < 1) {
-            throw new TypeError("nthIndex should starts with 1 in "+
+        if (nthIndex === 0) {
+            throw new TypeError("nthIndex cannot be 0 in "+
                 "String.nthIndexOf(searchValue, nthIndex, start)");
         }
-        tmp = this.indexOf(searchValue, start);
-        for(i=1,l=nthIndex; i<l; i++) {
-            tmp = this.indexOf(searchValue, tmp+1);
-            if (tmp === -1) {
-                return -1;
+        // initialize the loop
+        i=(typeof start === "undefined")? -1: start-1;
+        l=this.length;
+        /* 
+         * loop throught the string using the string length as iteration 
+         * condition
+         */
+        while(i<l) {
+            /*
+             * use indexOf with progressively increment i to search for 
+             * substring occurence 
+             */
+            i = this.indexOf(searchValue, i+1);
+            // position nthIndex reached
+            if (posIndex+1 === nthIndex) {
+                return i;
+            }
+            // no more occurence of the string found
+            if (i === -1) {
+                break;
+            }
+            // keep an array of position for negative nth index
+            position[posIndex++] = i;
+        }
+        if (nthIndex < 0) {
+            // calcuate the correct index for negative nth index
+            targetIndex = posIndex + nthIndex;
+            if (targetIndex >= 0) {
+                return position[targetIndex];
             }
         }
-        return tmp;
+        return -1;
     }
 }
 /**
@@ -48,27 +75,53 @@
  *
  * @param {string} str          The string to search with
  * @param {string} searchValue  The value to search for
- * @param {number} nthIndex     Which occurrence to search for, index
- *                              starts with st 1
+ * @param {number} nthIndex     Which occurence to search for. If index 
+ *                              is a negative number, it will perform
+ *                              a backward search. All index starts at
+ *                              1
  * @param {number} [start]      (Optional) At which position to start
  * @return {number}             Position where the search value of nth 
  *                              occurrence appears, -1 if it does not 
  *                              appears
  */
 function nthIndexOf(str, searchValue, nthIndex, start) {
-    var i, l, 
-        tmp;
+        var i, l, 
+            tmp, position=[], posIndex=0, targetIndex;
         
-    if (nthIndex < 1) {
-        throw new TypeError("nthIndex should starts with 1 in "+
-            "nthIndexOf(string, searchValue, nthIndex, start)");
-    }
-    tmp = str.indexOf(searchValue, start);
-    for(i=1,l=nthIndex; i<l; i++) {
-        tmp = str.indexOf(searchValue, tmp+1);
-        if (tmp === -1) {
-            return -1;
+        if (nthIndex === 0) {
+            throw new TypeError("nthIndex cannot be 0 in "+
+                "String.nthIndexOf(searchValue, nthIndex, start)");
         }
-    }
-    return tmp;
+        // initialize the loop
+        i=(typeof start === "undefined")? -1: start-1;
+        l=str.length;
+        /* 
+         * loop throught the string using the string length as iteration 
+         * condition
+         */
+        while(i<l) {
+            /*
+             * use indexOf with progressively increment i to search for 
+             * substring occurence 
+             */
+            i = str.indexOf(searchValue, i+1);
+            // position nthIndex reached
+            if (posIndex+1 === nthIndex) {
+                return i;
+            }
+            // no more occurence of the string found
+            if (i === -1) {
+                break;
+            }
+            // keep an array of position for negative nth index
+            position[posIndex++] = i;
+        }
+        if (nthIndex < 0) {
+            // calcuate the correct index for negative nth index
+            targetIndex = posIndex + nthIndex;
+            if (targetIndex >= 0) {
+                return position[targetIndex];
+            }
+        }
+        return -1;
 }
